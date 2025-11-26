16d3703a1d200dabe170382e0fde3c212b5a117d
SurajV Jadhav <surajv.10718576@ltimindtree.com>
2025-11-26 21:18:32 +0530

    StringPrograms

---
+++ src/main/java/string/FindDistinctCharacter.java
@@ -0,0 +1,26 @@
+package string;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class FindDistinctCharacter {
+    public static void main(String[] args) {
+        String s= "Resposible";
+        
+        for(int i=0;i<s.length();i++) {
+        char ch= s.charAt(i);
+        int count=0;
+        for(int j=0;j<s.length();j++) {
+        	if(ch==s.charAt(j)) {
+        		count++;
+        	}
+        }
+        if(count==1) {
+        	System.out.print(ch+ "");
+        }
+        }
+    }
+}
+
+        
---
+++ src/main/java/string/FindLenthofString.java
@@ -0,0 +1,15 @@
+package string;
+
+public class FindLenthofString {
+public static void main(String[] args) {
+	String S="Suraj";
+	char[] ch = S.toCharArray();
+	int lenght=0;
+	for(char c:ch) {
+		lenght++;
+	}
+	
+	System.out.println("lenght Of String is : "+lenght);
+}
+}
+
